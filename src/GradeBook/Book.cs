7cf2ab2eea5dbc7670601bc8abc56bff03cf33b3
Alexandre Pedrecal <pedrecalc@gmail.com>
2020-03-07 10:53:18 -0300

    :recycle: (refac to statistics obj instead of printing the statistics)
    Class 38

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -3,38 +3,39 @@ using System.Collections.Generic;
 
 namespace GradeBook
 {
-  public class Book
-  {
-    public Book(string name)
-    {
-      grades = new List<double>();
-      this.name = name;
-    }
+	public class Book
+	{
+		public Book(string name)
+		{
+			grades = new List<double>();
+			this.name = name;
+		}
 
-    public void AddGrade(double grade)
-    {
-      grades.Add(grade);
-    }
+		public void AddGrade(double grade)
+		{
+			grades.Add(grade);
+		}
 
-    public void ShowStatistics()
-    {
-      var average = 0.0;
-      var highGrade = double.MinValue;
-      var lowGrade = double.MaxValue;
+		public Statistics GetStatistics()
+		{
+			var result = new Statistics();
 
-      foreach (var grade in grades)
-      {
-        lowGrade = Math.Min(grade, lowGrade);
-        highGrade = Math.Max(grade, highGrade);
-        average += grade;
-      }
-      average /= grades.Count;
-      Console.WriteLine($"The lowest grade is {lowGrade}");
-      Console.WriteLine($"The highest grade is {highGrade}");
-      Console.WriteLine($"The average grade is {average:N1}");
-    }
+			result.Average = 0.0;
+			result.High = double.MinValue;
+			result.Low = double.MaxValue;
 
-    private List<double> grades;
-    private string name;
-  }
+			foreach (var grade in grades)
+			{
+				result.Low = Math.Min(grade, result.Low);
+				result.High = Math.Max(grade, result.High);
+				result.Average += grade;
+			}
+			result.Average /= grades.Count;
+
+			return result;
+		}
+
+		private List<double> grades;
+		private string name;
+	}
 }
