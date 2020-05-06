7765d681c910dab56d1b167c5d23acbf7067217b
Alexandre Pedrecal Campos <pedrecalc@gmail.com>
2020-05-06 12:06:42 -0300

    finish: testing and ref types

--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -3,32 +3,93 @@ using Xunit;
 
 namespace GradeBook.Tests
 {
-	public class TypeTests
-	{
-		[Fact]
-		public void GetBookReturnsDifferentObjects()
-		{
-			var book1 = GetBook("Book 1");
-			var book2 = GetBook("Book 2");
+  public class TypeTests
+  {
+    [Fact]
+    public void Test1()
+    {
+      var x = GetInt();
+      SetInt(ref x);
 
-			Assert.Equal("Book 1", book1.Name);
-			Assert.Equal("Book 2", book2.Name);
-			Assert.NotSame(book1, book2);
-		}
+      // Assert.Equal(3, x);
+    }
 
-		[Fact]
-		public void TwoVarsCanReferenceSameObject()
-		{
-			var book1 = GetBook("Book 1");
-			var book2 = book1;
+    private void SetInt(ref int x)
+    {
+      x = 42;
+    }
 
-			Assert.Same(book1, book2);
-			Assert.True(Object.ReferenceEquals(book1, book2));
-		}
+    private int GetInt()
+    {
+      return 3;
+    }
 
-		Book GetBook(string name)
-		{
-			return new Book(name);
-		}
-	}
+    [Fact]
+    public void CSharpCanPassByRef()
+    {
+      var book1 = GetBook("Book 1");
+      GetBookSetName(ref book1, "New Name");
+
+      Assert.Equal("New Name", book1.Name);
+    }
+
+    private void GetBookSetName(ref Book book, string name)
+    {
+      book = new Book(name);
+    }
+
+    [Fact]
+    public void CSharpIsPassByValue()
+    {
+      var book1 = GetBook("Book 1");
+      GetBookSetName(book1, "New Name");
+
+      Assert.Equal("Book 1", book1.Name);
+    }
+
+    private void GetBookSetName(Book book, string name)
+    {
+      book = new Book(name);
+    }
+
+    [Fact]
+    public void CanSetNameFromReference()
+    {
+      var book1 = GetBook("Book 1");
+      SetName(book1, "New Name");
+
+      Assert.Equal("New Name", book1.Name);
+    }
+
+    private void SetName(Book book, string name)
+    {
+      book.Name = name;
+    }
+
+    [Fact]
+    public void GetBookReturnsDifferentObjects()
+    {
+      var book1 = GetBook("Book 1");
+      var book2 = GetBook("Book 2");
+
+      Assert.Equal("Book 1", book1.Name);
+      Assert.Equal("Book 2", book2.Name);
+      Assert.NotSame(book1, book2);
+    }
+
+    [Fact]
+    public void TwoVarsCanReferenceSameObject()
+    {
+      var book1 = GetBook("Book 1");
+      var book2 = book1;
+
+      Assert.Same(book1, book2);
+      Assert.True(Object.ReferenceEquals(book1, book2));
+    }
+
+    Book GetBook(string name)
+    {
+      return new Book(name);
+    }
+  }
 }
